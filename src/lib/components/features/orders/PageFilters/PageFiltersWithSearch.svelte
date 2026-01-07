5739231b893016e15d1dab172fb7385dfb038608
Lorenzo Fontana <gufoscuro@gmail.com>
2026-01-07 16:15:24 +0100

    chore: Add some components

---
+++ src/lib/components/features/orders/PageFilters/PageFiltersWithSearch.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Input from '$lib/components/ui/input/input.svelte';
+</script>
+
+<div class="flex w-full items-center justify-between gap-4">
+	<div>Page Filters With Search Component</div>
+
+	<Input type="search" placeholder="Email" class="max-w-xs" />
+</div>
---
+++ src/lib/components/features/orders/PageFilters/default/PageFilters.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<div class="flex w-full items-center gap-4">Page Filters Component</div>
