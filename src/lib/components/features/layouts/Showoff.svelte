192c6ac22226796c35881708a940e54afa2f21e7
Lorenzo Fontana <gufoscuro@gmail.com>
2026-01-15 19:31:00 +0100

    chore: Add showoff layout file

---
+++ src/lib/components/features/layouts/Showoff.svelte
@@ -0,0 +1,25 @@
+<!--
+/**
+ * @description Showoff layout component that renders a page with a title and a central component to showcase content
+ */
+-->
+
+<script lang="ts">
+  import AppHeader from '$components/features/globals/AppHeader.svelte'
+  import SnippetResolver from '$components/runtime/SnippetResolver.svelte'
+  import type { SnippetProps } from '$utils/runtime'
+
+  type ShowoffLayoutProps = SnippetProps & {}
+
+  const { pageDetails }: ShowoffLayoutProps = $props()
+  const { config } = $derived(pageDetails)
+</script>
+
+<AppHeader />
+
+<main class="flex flex-1 flex-col gap-4 px-4">
+  <SnippetResolver snippet={config.snippets.title} class="h-11 w-full" />
+  <div class="flex flex-1 items-center justify-center">
+    <SnippetResolver snippet={config.snippets.target} />
+  </div>
+</main>
